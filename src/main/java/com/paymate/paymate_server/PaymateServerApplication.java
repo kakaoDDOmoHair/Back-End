7fd31ac3297205cdae7f043763647d9be1747a30
kimdohong <rlaehghd12@daum.net>
2026-01-16 13:17:14 +0900

    project init

---
+++ src/main/java/com/paymate/paymate_server/PaymateServerApplication.java
@@ -0,0 +1,13 @@
+package com.paymate.paymate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymate/paymate_server/PaymateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymate.paymate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
