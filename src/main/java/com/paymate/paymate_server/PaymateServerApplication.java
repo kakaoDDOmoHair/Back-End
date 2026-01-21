a30b944bdd626aec5180358a283dc7f57a14d283
jjy0701 <86817506+jjy0701@users.noreply.github.com>
2026-01-21 10:34:20 +0900

    실행급여 정산을 위í한 추가

--- src/main/java/com/paymate/paymate_server/PaymateServerApplication.java
+++ src/main/java/com/paymate/paymate_server/PaymateServerApplication.java
@@ -2,8 +2,12 @@ package com.paymate.paymate_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAsync
 @SpringBootApplication
+@EnableJpaAuditing
 public class PaymateServerApplication {
 
 	public static void main(String[] args) {
