57e8e4033957cbb7fbc51fc8dfa9af897628c707
jjy0701 <86817506+jjy0701@users.noreply.github.com>
2026-01-22 14:55:42 +0900

    로그인 요청 ì½로직 추가

--- src/main/java/com/paymate/paymate_server/domain/auth/dto/LoginRequestDto.java
+++ src/main/java/com/paymate/paymate_server/domain/auth/dto/LoginRequestDto.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class LoginRequestDto {
-    private String email;
+    private String username;
     private String password;
 }
